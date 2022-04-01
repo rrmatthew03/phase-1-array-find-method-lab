980dee3fde80df121b5e04c36911e866c98973d1
ryan rivera <rrmatthew03@yahoo.com>
2022-04-01 07:41:21 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function superbowlWin(record){
+      function isWinningYear(year) {
+        if (year.result === "W") {
+          return true
+        } else {
+          return false
+        }
+      }
+  const winningYearObject = record.find(isWinningYear)
+      if (winningYearObject) {
+        return winningYearObject.year
+      }
+    }
